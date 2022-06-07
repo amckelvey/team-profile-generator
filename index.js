f03a23f45d54019142a1425011e4ee0ea2f6cb3e
Majelvey <mckelvey.andy@gmail.com>
2022-06-07 17:38:20 -0400

    added basic inquirer prompt structure to index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Dependencies
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Inquirer prompts
+inquirer
+    .prompt([
+
+    ])
+    .then((data) => {
+        
+    }
+    );
